3f72ce82e81364658003f1c1f1572d08dda898ac
Abhishek Maan <AbhishekMaan1997@gmail.com>
2019-06-29 13:51:51 -0400

    Got the css and index.html to display the webpage as shown in example

---
+++ assets/Javascript/app.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB3dJ_Z82BPXUdl34OzZZbfOL4XbYVAAqM",
+    authDomain: "trainschedule-2f5f8.firebaseapp.com",
+    databaseURL: "https://trainschedule-2f5f8.firebaseio.com",
+    projectId: "trainschedule-2f5f8",
+    storageBucket: "",
+    messagingSenderId: "141419451540",
+    appId: "1:141419451540:web:55d307c7ab50b48c"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+var database = firebaseConfig.database();
